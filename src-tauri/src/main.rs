daa08d3feceb200f766199912fb2e85516a7dad5
Daniel Heater <daniel@heatertech.com>
2022-07-02 17:07:37 -0500

    Make the tray menu quit button exit the app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,22 +3,25 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::SystemTray;
-use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
 
 fn main() {
-    let context = tauri::generate_context!();
-
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
     let tray_menu = SystemTrayMenu::new()
         .add_item(quit)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(hide);
-    let tray = SystemTray::new().with_menu(tray_menu);
+        .add_native_item(SystemTrayMenuItem::Separator);
 
     tauri::Builder::default()
-        .system_tray(tray)
-        .run(context)
-        .expect("error while running tauri application");
+        .system_tray(SystemTray::new().with_menu(tray_menu))
+        .on_system_tray_event(|_app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                _ => {}
+            },
+            _ => {}
+        })
+        .run(tauri::generate_context!())
+        .expect("Error while running Tauri system tray application");
 }
