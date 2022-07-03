f4e303cecd496522964aa800f2660b758ce27f80
Daniel Heater <daniel@heatertech.com>
2022-07-02 19:03:32 -0500

    Add the app version number from Cargo.toml to the tray menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,11 +5,15 @@
 
 use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let version = CustomMenuItem::new("version".to_string(), "Version: ".to_string() +VERSION);
     let tray_menu = SystemTrayMenu::new()
         .add_item(quit)
-        .add_native_item(SystemTrayMenuItem::Separator);
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(version.disabled());
 
     tauri::Builder::default()
         .system_tray(SystemTray::new().with_menu(tray_menu))
